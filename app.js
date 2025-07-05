70e7b346cd59ba0bf54f4ac32ba876e772ea0299
sweeneyb <tab126@gmail.com>
2025-07-05 17:55:53 -0400

    add oscilliscope for debugging

--- app.js
+++ app.js
@@ -81,6 +81,44 @@ async function startListening() {
 
   const buffer = new Float32Array(analyser.fftSize);
 
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  analyser.getByteTimeDomainData(dataArray);
+  const canvas = document.getElementById("oscilloscope");
+  const canvasCtx = canvas.getContext("2d");
+
+  function draw() {
+  
+    analyser.getByteTimeDomainData(dataArray);
+  
+    canvasCtx.fillStyle = "rgb(200 200 200)";
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = "rgb(0 0 0)";
+  
+    canvasCtx.beginPath();
+  
+    const sliceWidth = (canvas.width * 1.0) / bufferLength;
+    let x = 0;
+  
+    for (let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = (v * canvas.height) / 2;
+  
+      if (i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+  
+      x += sliceWidth;
+    }
+  
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+  }
+
   function autoCorrelate(buf, sampleRate) {
     let bestOffset = -1;
     let bestCorrelation = 0;
@@ -118,6 +156,7 @@ async function startListening() {
   }
 
   const detect = () => {
+    draw()
     analyser.getFloatTimeDomainData(buffer);
     const freq = autoCorrelate(buffer, audioContext.sampleRate);
     console.log("freq: ", freq)
